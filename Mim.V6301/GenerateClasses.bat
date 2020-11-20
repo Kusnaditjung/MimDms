dbd43182d18a2bc9b0d1959bcaf0e8b356cf0d9d
kusna <kusna@LAPTOP-TBMCHFI2>
2020-11-20 09:26:06 +0000

    Add MIM 4.2.00

---
+++ Mim.V6301/GenerateClasses.bat
@@ -0,0 +1,19 @@
+﻿xs@echo off
+
+
+set Schemapath=Schema
+set Outputpath=../../Generated
+set Namepl=Mim.V6301
+
+echo.Starting processing MIM XSD files ...
+for /f %%a IN ('dir %Schemapath%\*.xsd /a-d /b /s') do call:ProcessSchema %%a
+
+echo.Finished processing XSD files ...
+echo.&pause&
+goto:eof
+
+
+:ProcessSchema
+Xsd2Code %~1 %Namepl% %Outputpath% /m /pl Net35 /if- /xa /sc /is /cl /c Array /eit /sdefault+ /sskipnull+
+echo.Processed %~n1
+goto:eof
