c37d8ea87ff84ace94566e2a09489bf62914c7ad
kusna <kusna@LAPTOP-TBMCHFI2>
2021-01-10 15:09:24 +0000

    Add ambulance DMS 2.1.0

---
+++ Dms.Ambulance.V2100/GenerateClasses.bat
@@ -0,0 +1,18 @@
+﻿xs@echo off
+
+set Schemapath=Schema
+set Outputpath=../../Generated
+set Namepl=Dms.Ambulance.V2100
+
+echo.Starting processing MIM XSD files ...
+for /f %%a IN ('dir %Schemapath%\*.xsd /a-d /b /s') do call:ProcessSchema %%a
+
+echo.Finished processing XSD files ...
+echo.&pause&
+goto:eof
+
+
+:ProcessSchema
+Xsd2Code %~1 %Namepl% %Outputpath% /m /pl Net35 /if- /xa /sc /is /cl /c Array /eit /sdefault+ /sskipnull+
+echo.Processed %~n1
+goto:eof
--- Mim.V4200/README.md
+++ Mim.V4200/README.md
@@ -1,4 +1,4 @@
 ﻿# Generated classes for NHS Message Information Manual version 4.2.00
 - Some change to the original MIM 4.2.00
-  - the voc/voc.xsd missing inclusion of "dt/datatypes.xsd"
+  - the voc/voc.xsd missing inclusion of "../dt/datatypes.xsd"
   - the dt/infrastructureRoot.xsd inclusion of datatypes should be "datatypes.xsd" instead of "./datatypes.xsd"
